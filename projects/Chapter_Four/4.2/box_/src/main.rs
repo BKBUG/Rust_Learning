feeeeaab7e9a87034d7ad03fcca492ad586e2a03
chuCrazyMan <2595456250@qq.com>
2024-03-24 22:13:43 +0800

    New Chapter_Four

---
+++ projects/Chapter_Four/4.2/box_/src/main.rs
@@ -0,0 +1,39 @@
+/*
+fn main()
+{
+	let a_num = 4;
+	make_and_drop();
+}
+
+fn make_and_drop()
+{
+	let a_box = Box::new(5);
+}
+*/
+
+/*
+fn main()
+{
+	let first = String::from("Ferris");
+	let full = add_suffix(first);
+	println!("{full}");
+}
+
+fn add_suffix(mut name: String) -> String
+{
+	name.push_str(" Jr.");
+	name
+}
+*/
+fn main() 
+{
+ 	let first = String::from("Ferris");
+	let first_clone = first.clone();
+	let full = add_suffix(first_clone);
+	println!("{full}, originally {first}");
+}
+fn add_suffix(mut name:String) -> String
+{
+	name.push_str(" Jr.");
+	name
+}
---
+++ projects/Chapter_Four/4.2/test_/src/main.rs
@@ -0,0 +1,16 @@
+fn main() 
+{
+	let s = String::from("hello");
+	let s2;
+	let b = false;
+
+	if b {
+		s2 = s;	
+		println!{"here expression in if "};
+	}
+	else {
+		println!{"Here expression in else"};
+	}
+
+//	println!("{}", s);
+}
